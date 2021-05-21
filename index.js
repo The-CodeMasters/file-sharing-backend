168c648c25499ed90237764296b888bc8d00e7ef
yash nathwani <yashsnathwani15@gmail.com>
2021-05-21 23:01:10 +0530

    added git ignore

--- index.js
+++ index.js
@@ -10,59 +10,7 @@ const PORT = 8080;
 let ActiveDir = `/media/${CURRENT_USER}`;
 const HOME = os.homedir();
 
-function isDir(path) {
-  try {
-    var stat = fs.lstatSync(path);
-    return stat.isDirectory();
-  } catch (e) {
-    // lstatSync throws an error if path doesn't exist
-    return false;
-  }
-}
-const convertBytes = function (bytes) {
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-
-  if (bytes == 0) {
-    return "n/a";
-  }
-
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-
-  if (i == 0) {
-    return bytes + " " + sizes[i];
-  }
-
-  return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i];
-};
-const getAllFiles = function (dirPath, arrayOfFiles) {
-  files = fs.readdirSync(dirPath);
-
-  arrayOfFiles = arrayOfFiles || [];
-
-  files.forEach(function (file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
-    } else {
-      arrayOfFiles.push(path.join(dirPath, file));
-    }
-  });
-
-  return arrayOfFiles;
-};
-const getDirTotalSize = function (directoryPath) {
-  const arrayOfFiles = getAllFiles(directoryPath);
-
-  let totalSize = 0;
-
-  arrayOfFiles.forEach(function (filePath) {
-    totalSize += fs.statSync(filePath).size;
-  });
-
-  return totalSize;
-};
-
-/* ***************************************************************** */
-console.log(os.homedir());
+const { isDir, convertBytes, getDirTotalSize } = require("./util/UtilFunction");
 
 app.get("", (req, res) => {
   fs.readdir(ActiveDir, function (err, files) {
